5d9a2dc8ede74d2b7327ff6d558dccf203354193
tao.yu <zale.yu@tinno.com>
2016-09-27 15:32:19 +0800

    < init project : square path>

---
+++ app/src/main/java/com/ted/myvectordrawable/MainActivity.java
@@ -0,0 +1,29 @@
+package com.ted.myvectordrawable;
+
+import android.graphics.drawable.Animatable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onSquareClick(View view) {
+        startAnim(view);
+    }
+
+    private void startAnim(View view) {
+        ImageView imageView = (ImageView)view;
+        Animatable animatable = (Animatable) imageView.getDrawable();
+        if (animatable.isRunning()){
+            animatable.start();
+        }
+        animatable.start();
+    }
+}
